297e6825ab0d8d899ae932e813eb2cb08fc67e20
Cauã Gomes <cauagomespereira@gmail.com>
2024-07-19 21:09:32 -0300

    first commit

---
+++ src/componentes/Banner/Banner/Banner.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react'
+import './Banner.css'
+
+export const Banner = () => {
+    
+    return (
+        <Fragment>
+            <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Ornago"/>
+            </header>
+        </Fragment>
+    )
+
+}
